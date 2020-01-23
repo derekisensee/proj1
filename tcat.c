4a08803e00d32e8535e34121696af0515b7131bf
derek <derek@debian>
2020-01-23 11:32:45 -0600

    Finish tcat.c?

--- tcat.c
+++ tcat.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX_LENGTH 1000
 int main(int argc, char* argv[]) {
+  if (argc == 1) { // if no args given
+    printf("Arg error! No args given.\n");
+    return 0;
+  }
+
   FILE *fp;
-  if ((fp = fopen("nameGoesHere", "r")) == NULL) {
+  if ((fp = fopen(argv[1], "r")) == NULL) {
     printf("File error! It doesn't exist :(\n");
 
     exit(1);
   }
 
-  fscanf(fp
-  
+  //char * line = NULL;
+  char line[1000];
+  size_t len = 0;
+  ssize_t read;
+
+  /*while ((read = getline(&line, &len, fp)) != -1) {
+    printf("%s", line);
+  }*/
+
+  while (fgets(line, MAX_LENGTH, fp) != NULL)
+    printf("%s", line);
+
+  fclose(fp);
   
   return 0;
 }
