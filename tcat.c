779028749f345619a81570c67ef2a3ef08f1af41
derek <derek@debian>
2020-01-22 22:45:13 -0600

    add tcat.c

---
+++ tcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  FILE *fp;
+  if ((fp = fopen("nameGoesHere", "r")) == NULL) {
+    printf("File error! It doesn't exist :(\n");
+
+    exit(1);
+  }
+
+  fscanf(fp
+  
+  
+  return 0;
+}
