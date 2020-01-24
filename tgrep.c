54699e94c49c5a939dfadc0899ac4894626ae792
derek <derek@debian>
2020-01-23 21:42:42 -0600

    start tgrep.c

---
+++ tgrep.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LENGTH 1000
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) { // if no args passed
+    printf("tgrep: searchterm [file...]\n");
+  }
+
+  // here: read from stdin (keyboard)
+  if (argc == 2) {
+
+  }
+
+  if (argc == 3) {
+    
+  }
+}
