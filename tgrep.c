63f9b2449f741de80ba88b7ff7768ffdc405aa4e
derek <derek@debian>
2020-01-23 22:20:16 -0600

    continue tgrep.c

--- tgrep.c
+++ tgrep.c
@@ -7,12 +7,31 @@ int main(int argc, char* argv[]) {
     printf("tgrep: searchterm [file...]\n");
   }
 
-  // here: read from stdin (keyboard)
+  // searching from keyboard input (stdin)
   if (argc == 2) {
 
   }
 
+  // searching in a file
   if (argc == 3) {
+    char *searchWord = argv[1]; // the string we're searching for
+    FILE *fp; // our file
+    fp = fopen(argv[2], "r");
+    if (fp == NULL) {
+      printf("tgrep: cannot open file\n");
+      exit(1);
+    }
     
+    // line stuff
+    char *buffer;
+    size_t buffsize = 32;
+    size_t chars;
+
+    buffer = (char *)malloc(buffsize * sizeof(char));
+    chars = getline(&buffer, &buffsize, fp);
+    // ? now what ?
+
+    // ... and close.
+    fclose(fp);
   }
 }
